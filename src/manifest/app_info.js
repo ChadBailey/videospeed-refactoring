5df14a022f7601cc0c2cb04a02d8395c0950c4ef
Chad Bailey <chadbailey904@gmail.com>
2022-09-06 01:29:59 -0500

    Added BETA markings to names and description

--- src/manifest/app_info.js
+++ src/manifest/app_info.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: "Video Speed Controller",
-  short_name: "videospeed",
+  name: "Video Speed Controller - BETA Channel",
+  short_name: "videospeed-controller-beta",
   description:
-    "Speed up, slow down, advance and rewind HTML5 audio/video with shortcuts"
+    "BETA testing release of Video Speed Controller. Speed up, slow down, advance and rewind HTML5 audio/video with shortcuts"
 };
