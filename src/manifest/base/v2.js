f3d03493fcba107fec30b2918650e887f3909c51
Chad Bailey <chadbailey904@gmail.com>
2022-09-11 23:04:07 -0500

    Fix script: build #10 fixed

--- src/manifest/base/v2.js
+++ src/manifest/base/v2.js
@@ -1,21 +1,28 @@
-const { version } = require("../version.json");
 const permissions = require("../permissions");
-const { name, short_name, description } = require("../app_info");
+const npm_package = require("../../../package.json");
 
 module.exports = {
-  version,
+  name: npm_package.long_name, // Shows in extension icon right-click menu
+  version: npm_package.version,
   manifest_version: 2,
-  name,
-  short_name,
-  description,
+  description: npm_package.description,
+  homepage_url: npm_package.homepage, // Linked on clicking name in extension icon right-click menu
   permissions: [...permissions, "https://*/*"],
   browser_action: {
-    default_title: short_name,
+    default_title: npm_package.name,
     default_popup: "assets/html/popup.html",
   },
   content_scripts: [
     {
+      all_frames: true,
       matches: ["<all_urls>"],
+      match_about_blank: true,
+      exclude_matches: [
+        "https://plus.google.com/hangouts/*",
+        "https://hangouts.google.com/*",
+        "https://meet.google.com/*",
+      ],
+      css: ["assets/css/content.css"],
       js: ["content.js"],
     },
   ],
@@ -27,7 +34,13 @@ module.exports = {
   background: {
     scripts: ["background.js"],
   },
-  web_accessible_resources: ["assets/**"],
+  web_accessible_resources: [
+    "assets/css/content.css", // CSS fixes for many pages such as YouTube
+    "background.js.map", // Maps to allow "real" line and column numbers/debugging
+    "content.js.map",
+    "options.js.map",
+    "popup.js.map",
+  ],
   // ...(process.env.NODE_ENV === 'development' ? {
   //   content_security_policy: "script-src 'self' 'unsafe-eval'; font-src 'self' data: https://fonts.gstatic.com/s/dmsans; object-src 'self';"
   // } : {})
