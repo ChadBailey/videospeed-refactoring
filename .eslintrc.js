4413c654f3c02357e34a73172fa04190adcc923c
Chad Bailey <chadbailey904@gmail.com>
2022-09-14 01:27:55 -0500

    Reformatted .eslintrc.js as ES6 module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,31 @@
-module.exports = {
-  plugins: ["jest"],
-  parserOptions: {
-    ecmaVersion: "2020",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-    env: {
-      webextensions: true,
-      es2020: true,
-      "shared-node-browser": true,
-    },
-    extends: "eslint:recommended",
+export const plugins = ["jest"];
+export const parserOptions = {
+  ecmaVersion: "2020",
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true,
+  },
+  env: {
+    webextensions: true,
+    es2020: true,
+    "shared-node-browser": true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    indent: [2, "error"],
+    semi: [2, "always"],
+  },
+};
+export const overrides = [
+  {
+    files: ["./src/**/*.{test,spec}.{js,jsx}", "./test/**/*.{js,jsx}"],
     rules: {
-      indent: [2, "error"],
-      semi: [2, "always"],
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
   },
-  overrides: [
-    {
-      files: ["./src/**/*.{test,spec}.{js,jsx}", "./test/**/*.{js,jsx}"],
-      rules: {
-        "jest/no-disabled-tests": "warn",
-        "jest/no-focused-tests": "error",
-        "jest/no-identical-title": "error",
-        "jest/prefer-to-have-length": "warn",
-        "jest/valid-expect": "error",
-      },
-    },
-  ],
-};
+];
